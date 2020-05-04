1bc4712187b54f88cbdf7d34f6a4060602b8d5d2
Stefan Dworschak <stef.dworschak@gmail.com>
2020-05-04 16:11:37 +0100

    hyperion_web/settings.py

--- hyperion_web/settings.py
+++ hyperion_web/settings.py
@@ -27,10 +27,13 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+EC2_HOSTNAME = os.environ.get('EC2_HOSTNAME')
+
 ALLOWED_HOSTS = [
     'https://hyperion-app.herokuapp.com/',
     'hyperion-app.herokuapp.com',
-    'localhost'
+    'localhost',
+    EC2_HOSTNAME
     ]
 
 
