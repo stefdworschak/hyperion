88120d3dd9fb92803b64f0fc26a3c07c9032ed75
Ubuntu <ubuntu@ip-172-31-5-234.eu-west-1.compute.internal>
2020-05-07 21:55:11 +0000

    Changing templates dirs

--- hyperion_web/settings.py
+++ hyperion_web/settings.py
@@ -15,7 +15,7 @@ import env_vars
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     #Custom Apps
     'checkin',
     'healthcare',
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'hyperion_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
